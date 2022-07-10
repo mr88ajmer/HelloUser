c10cfe0808a12898fc77ff9af3e97d04c131a3d9
EvgeniyBasht <ajmer@yandex.ru>
2022-07-10 12:52:37 +0300

    Аргументы внутри  функции максимума Max

--- Leksiya2_1/Program.cs
+++ Leksiya2_1/Program.cs
@@ -7,7 +7,7 @@
 }
 
 
-int a1 = 15;
+int a1 = 112125;
 int b1 = 21;
 int c1 = 39;
 
@@ -19,9 +19,14 @@ int a3 = 13;
 int b3 = 23111;
 int c3 = 313;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
