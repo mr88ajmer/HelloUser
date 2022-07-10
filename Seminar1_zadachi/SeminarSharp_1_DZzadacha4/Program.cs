b0cc15d529f0bbe9a376d2bc3bf6b01294ddc072
EvgeniyBasht <ajmer@yandex.ru>
2022-07-10 19:37:39 +0300

    семинар1 задача 4 выполнена

---
+++ Seminar1_zadachi/SeminarSharp_1_DZzadacha4/Program.cs
@@ -0,0 +1,47 @@
+﻿int a1 = 2;
+int b1 = 3;
+int c1 = 7;
+if (a1 > b1 && a1 > c1)
+{
+    Console.WriteLine(a1);
+}
+else if (b1 > c1)
+{
+    Console.WriteLine(b1);
+}
+else
+{
+    Console.WriteLine(c1);
+}
+
+int a2 = 44;
+int b2 = 5;
+int c2 = 78;
+if (a2 > b2 && a2 > c2)
+{
+    Console.WriteLine(a2);
+}
+else if (b2 > c2)
+{
+    Console.WriteLine(b2);
+}
+else
+{
+    Console.WriteLine(c2);
+}
+
+int a3 = 22;
+int b3 = 3;
+int c3 = 9;
+if (a3 > b3 && a3 > c3)
+{
+    Console.WriteLine(a3);
+}
+else if (b3 > c3)
+{
+    Console.WriteLine(b3);
+}
+else
+{
+    Console.WriteLine(c3);
+}
