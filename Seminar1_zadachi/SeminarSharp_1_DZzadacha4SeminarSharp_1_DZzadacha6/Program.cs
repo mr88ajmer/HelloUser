ce597a7f752070836a530a595326af89cb2b1026
EvgeniyBasht <ajmer@yandex.ru>
2022-07-10 22:16:32 +0300

    семинар1 задача 6 выполнена

--- Seminar1_zadachi/SeminarSharp_1_DZzadacha4SeminarSharp_1_DZzadacha6/Program.cs
+++ Seminar1_zadachi/SeminarSharp_1_DZzadacha4SeminarSharp_1_DZzadacha6/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a =4;
+int b = -3;
+int c = 7;
+if (a %2 == 0)
+{
+    Console.WriteLine("Число " + a + " является четным");
+}
+else 
+{
+    Console.WriteLine("Число " + a + " является нечетным");
+}
+
+if (b %2 == 0)
+{
+    Console.WriteLine("Число " + b + " является четным");
+}
+else
+{
+    Console.WriteLine("Число " + b + " является нечетным");
+}
+
+if (c %2 == 0)
+{
+    Console.WriteLine("Число " + c + " является четным");
+}
+else
+{
+    Console.WriteLine("Число " + c + " является нечетным");
+}
