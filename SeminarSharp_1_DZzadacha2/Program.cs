2806e2241103fd7d4964a44f5b673b45c5f35e03
EvgeniyBasht <ajmer@yandex.ru>
2022-07-10 18:38:37 +0300

    дз по семинару1 задача 2 выполнена

---
+++ SeminarSharp_1_DZzadacha2/Program.cs
@@ -0,0 +1,32 @@
+﻿int a1 = 5;
+int b1 = 7;
+if (a1 > b1)
+{
+    Console.WriteLine(a1);
+}
+else
+{
+    Console.WriteLine(b1);
+}
+
+int a2 = 2;
+int b2 = 10;
+if (a2 > b2)
+{
+    Console.WriteLine(a2);
+}
+else
+{
+    Console.WriteLine(b2);
+}
+
+int a3 = -9;
+int b3 = -3;
+if (a3 > b3)
+{
+    Console.WriteLine(a3);
+}
+else
+{
+    Console.WriteLine(b3);
+}
