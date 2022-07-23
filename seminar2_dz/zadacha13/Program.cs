c1273e07ec1e41de8974ffdf1d1203287d73f7ee
EvgeniyBasht <ajmer@yandex.ru>
2022-07-23 17:23:07 +0300

    семинар 2 задача 13 выполнены

---
+++ seminar2_dz/zadacha13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: 
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое число");
+string number = Console.ReadLine();
+if (number.Length > 2)
+{
+    char result = number[2];
+    Console.WriteLine($"Нашли третью цифру - это {result}");
+}else
+{
+    Console.WriteLine($"{number} - тут третьей цифры нет");
+}
