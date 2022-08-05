ddaa7107d8f3375e65d9abf23de168c994f7bfea
EvgeniyBasht <ajmer@yandex.ru>
2022-08-06 00:43:28 +0300

    семинар4 дз 27

---
+++ seminar4_dz/27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+//  и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число: "); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+int summa = 0; 
+while (number > 0)
+{ 
+    summa = summa + (number % 10);
+    number /=10;
+} 
+Console.WriteLine($"Сумма всех чисел: {summa}"); 
